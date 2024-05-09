51598b0c0c96be5e70d6fbc880bc4753acd6fa92
Louis <louis-kaan2002@web.de>
2024-05-09 10:49:00 +0200

    chore: removed button

--- apps/mobile/src/app/(auth)/(tabs)/notification.tsx
+++ apps/mobile/src/app/(auth)/(tabs)/notification.tsx
@@ -1,13 +1,10 @@
-import { router } from "expo-router";
-import { Button, Text } from "react-native";
+import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const NotificationPage = () => {
   return (
     <SafeAreaView>
-      <Text>
-        <Button title="welcome" onPress={() => router.navigate("/welcome")} />
-      </Text>
+      <Text>NotificationPage</Text>
     </SafeAreaView>
   );
 };
