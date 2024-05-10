d3afe9b09d25f0c3847f26fc7a59089adfcaf6a4
jrau1801 <janrau1802@gmail.com>
2024-05-10 15:48:48 +0200

    chore: added create, delete and get route for streams

--- apps/api/src/controller/stream.controller.ts
+++ apps/api/src/controller/stream.controller.ts
@@ -2,3 +2,7 @@ import type { Request, Response } from "express";
 import httpStatus from "http-status";
 import prismaClient from "../config/prisma";
 import { CreateUserCredentials, TypedRequest } from "../types/types";
+
+export const dummyfunc = () => {
+  return "0";
+};
