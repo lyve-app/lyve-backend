61398ae541c8c3addf0f4191fe25a1d935749eb7
jrau1801 <janrau1802@gmail.com>
2024-05-10 15:48:48 +0200

    chore: added create, delete and get route for streams

--- apps/api/src/routes/stream.route.ts
+++ apps/api/src/routes/stream.route.ts
@@ -3,4 +3,10 @@ import { streamController } from "../controller";
 
 const streamRouter = Router();
 
+streamRouter.post("/create", streamController.dummyfunc);
+
+streamRouter.get("/:id", streamController.dummyfunc);
+
+streamRouter.delete("/:id/delete", streamController.dummyfunc);
+
 export default streamRouter;
