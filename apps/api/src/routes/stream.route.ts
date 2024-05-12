26a8d8c939eee00e63fa80f206202dc05c7f96c8
jrau1801 <janrau1802@gmail.com>
2024-05-12 17:03:22 +0200

    feat: implemented GET /api/stream/:id closes #48

--- apps/api/src/routes/stream.route.ts
+++ apps/api/src/routes/stream.route.ts
@@ -5,7 +5,7 @@ const streamRouter = Router();
 
 streamRouter.post("/create", streamController.createStream);
 
-streamRouter.get("/:id", streamController.dummyfunc);
+streamRouter.get("/:id", streamController.getStreamInfo);
 
 streamRouter.delete("/:id/delete", streamController.dummyfunc);
 
