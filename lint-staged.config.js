853716d2319b5ff15ffe0b4458ad1dff030064bf
Louis <louis-kaan2002@web.de>
2024-05-14 19:41:18 +0200

    chore: moved lint staged conf to package.json

--- lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  "{apps,libs,tools}/**/*.{js,ts,jsx,tsx,json}": [
-    (files) => `yarn lint --files=${files.join(",")}`,
-    (files) => `yarn format --files=${files.join(",")}`
-  ]
-};
