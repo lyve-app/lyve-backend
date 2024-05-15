d80d283a37a574fa771190acb8849277b01f724a
jrau1801 <janrau1802@gmail.com>
2024-05-15 22:07:04 +0200

    chore: specified routes

--- apps/api/src/routes/stream.route.ts
+++ apps/api/src/routes/stream.route.ts
@@ -9,4 +9,10 @@ streamRouter.get("/:id", streamController.getStreamInfo);
 
 streamRouter.delete("/:id/delete", streamController.deleteStream);
 
+streamRouter.put("/:id/start", streamController.activateStream);
+
+streamRouter.put("/:id/end", streamController.endStream);
+
+streamRouter.put("/recommended", streamController.getRecommended);
+
 export default streamRouter;
