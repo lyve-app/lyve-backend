8e060e59aa693da498df49860a31566635c456b7
Louis <louis-kaan2002@web.de>
2024-05-22 12:41:37 +0200

    chore: updated rabbitmq config vars

--- apps/media-server/src/config/config.ts
+++ apps/media-server/src/config/config.ts
@@ -45,9 +45,9 @@ const config = {
     url: validatedEnv.RABBITMQ_URL,
     queues: {
       api_server_queue: "api_server_queue",
-      media_server_queue: "media_server_queue",
-      api_server_reply_queue: "api_server_reply_queue"
-    }
+      media_server_queue: "media_server_queue"
+    },
+    retryInterval: 5000
   },
   mediasoup: {
     worker: {
